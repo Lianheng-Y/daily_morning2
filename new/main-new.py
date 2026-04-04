ada10ff67a4abce4fc078a95a54911945f58ab56
Lianheng <168660314+Lianheng-Y@users.noreply.github.com>
2026-04-05 00:57:27 +0800

    Update main-new.py

--- new/main-new.py
+++ new/main-new.py
@@ -58,7 +58,7 @@ def get_one():
     # return response_data
 
 
-def send_message(to_user, access_token, city_name, weather, max_temperature, min_temperature):
+def send_message(to_user, access_token, weather, max_temperature, min_temperature):
     url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
     week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
     year = localtime().tm_year
