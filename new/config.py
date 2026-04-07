b90f16fe644eb26083c344f5f61f59e247129e6f
Lianheng <168660314+Lianheng-Y@users.noreply.github.com>
2026-04-07 16:49:10 +0800

    Update config.py

--- new/config.py
+++ new/config.py
@@ -4,7 +4,7 @@ app_id = "wx99ba32461bd2d00e"
 # 公众号appSecret
 app_secret = "a3d2932a8df068a1f16fe6cb4d5d1e35"
 # 模板消息id
-template_id = "jjVbeHYi6nAr2tS8eEE76h4q5zhTOiRDyqYnblrbcWI"
+template_id = "nc6hsUhYr7aA8dAq3KvYq3TbqfJk7ww-idAtOd4n1Sw"
 # 接收公众号消息的微信号
 # 这是我的
 users = "oPtu22HNpM9WSSpHajpRiX7O6stY"
